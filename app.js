c81f351c7d341c52d23c220b0577d9f05f2213c9
Mohamed Saleem <msaleemkp@hotmail.com>
2023-06-19 22:38:18 -0400

    feat: refactoring to move routes to its own folder

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
+const routes = require('./routes');
 const app = express();
-app.get('/', (request, response) => response.end('Hello World!')
-);
+routes(app);
+
 app.listen(3000, () => console.log('Application is running listening to 3000'));
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const appConfigs = require('../package.json');
+const routes = (expApp) => {
+    expApp.get('/', (request, response) => response.end('Hello World!') );
+    expApp.get('/app-config', (request, response) => {
+            const message = `
+            Application Name: ${appConfigs.name}
+            Application Version: ${appConfigs.version}`;
+            return response.end(message);
+        }
+    );
+}
+
+module.exports = routes;
