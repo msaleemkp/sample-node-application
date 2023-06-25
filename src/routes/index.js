6dace10e9a0be49b2f40e2872f8ea4228654a992
Mohamed Saleem <msaleemkp@hotmail.com>
2023-06-24 21:59:56 -0400

    feat: introduced folder structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-const appConfigs = require('../package.json');
+const appConfigs = require('../../package.json');
 const routes = (expApp) => {
     expApp.get('/', (request, response) => response.end('Hello World!') );
     expApp.get('/app-config', (request, response) => {
