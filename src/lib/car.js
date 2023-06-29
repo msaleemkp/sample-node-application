2473fc82ff1956a4424f07f32d2aab8fbd945646
msaleemkp <38106354+msaleemkp@users.noreply.github.com>
2023-06-28 23:09:01 -0400

    Feat/more apis (#5)
    * feat: refactoring to move routes to its own folder
    
    * feat: introduced folder structure
    
    * feat: added new apis
    
    * feat: adding the new folders and files
    
    ---------
    
    Co-authored-by: Mohamed Saleem <msaleemkp@hotmail.com>

---
+++ src/lib/car.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const { resolve } = require('path');
+
+const getDataFilePath = () => {
+    return resolve(appSetting.basePath + '/data/cars/stock.json');
+};
+
+const listCars = (cb) => {
+    fs.readFile(getDataFilePath(), (err, data) => {
+        if (err) {
+            cb(null, err);
+        } else {
+            const carList = JSON.parse(data);
+            cb(carList, null);
+        }
+    });
+};
+const addCar = (request, cb) => {
+    listCars((data, error) => {
+        if (error) {
+            cb(error, null);
+        } else {
+            const updatedData = [...data.items, request.body].map((car, index) => {
+                    return { 
+                        id: index +1,
+                        ...car, 
+                    }
+                });
+            const updatedStock = {"name": "Cars list", "items": updatedData};
+            const requestData = JSON.stringify(updatedStock, null, 2);
+            fs.writeFile(getDataFilePath(), requestData, (err) => {
+                if (err) {
+                    cb(err, null);
+                } else {
+                    cb(null, updatedStock);
+                }  
+            });
+        }
+    });
+}
+
+module.exports = {
+    listCars,
+    addCar,
+};
