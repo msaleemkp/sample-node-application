3e5f157fe5bfb8038d5b00e49afef99bafb6f2cc
msaleemkp <38106354+msaleemkp@users.noreply.github.com>
2023-06-28 23:03:07 -0400

    Feat/more apis (#4)
    * feat: refactoring to move routes to its own folder
    
    * feat: introduced folder structure
    
    * feat: added new apis
    
    ---------
    
    Co-authored-by: Mohamed Saleem <msaleemkp@hotmail.com>

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const routes = require('./src/routes');
 const routes = require('./routes');
 
 const app = express();
+global.appSetting = {
+    basePath: __dirname,
+}
+
+app.use(express.json());
 routes(app);
 
 app.listen(3000, () => console.log('Application is running listening to 3000'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
 const appConfigs = require('../../package.json');
+const carsRoutes = require('./commodities/cars');
 const routes = (expApp) => {
-    expApp.get('/', (request, response) => response.end('Hello World!') );
-    expApp.get('/app-config', (request, response) => {
-            const message = `
-            Application Name: ${appConfigs.name}
-            Application Version: ${appConfigs.version}`;
-            return response.end(message);
-        }
-    );
-}
+  carsRoutes(expApp);
+  expApp.get('/', (request, response) => response.end('Hello World!') );
+  expApp.get('/app-config', (request, response) => {
+          const message = `
+          Application Name: ${appConfigs.name}
+          Application Version: ${appConfigs.version}`;
+          return response.end(message);
+  });
+};
 
 module.exports = routes;
