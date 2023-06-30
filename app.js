7ab74fa3e33e495f015d9bcfe71d877f0aa8e458
msaleemkp <38106354+msaleemkp@users.noreply.github.com>
2023-06-29 22:16:27 -0400

    Feat/fix merge issues (#6)
    * feat: resolved merge conflict
    
    * chore: Updated version number
    
    ---------
    
    Co-authored-by: Mohamed Saleem <msaleemkp@hotmail.com>

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const routes = require('./src/routes');
-const routes = require('./routes');
 
-const app = express();
 global.appSetting = {
     basePath: __dirname,
 }
 
+const app = express();
 app.use(express.json());
 routes(app);
-
 app.listen(3000, () => console.log('Application is running listening to 3000'));
